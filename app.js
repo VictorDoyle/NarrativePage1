fe186277478e6fc5e7d1558ff575d60ba5ffe892
victordoyle <victor-doyle@outlook.com>
2020-12-30 18:51:58 -0500

    restyling needed + dialogue box added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 
 /* NOTE: Hidden Elements pre-loaded */
 $(".chapter1PartOne").hide();
+$(".dialogueBoxStripe").hide()
+$(".introductionImageChap1").hide()
 
 /* added game soundtrack to work on click */
 let chapter1Soundtrack = document.getElementById("purpleSong");
@@ -16,35 +18,8 @@ $(".beginChapter1").on("click", function fadeInChapter1(){
     playAudio();
     $(".chapter1Intro").fadeOut(1000);
     $(".chapter1PartOne").fadeIn(1000);
-    /* first column */
-    $("#partOneColumn1").css({
-        "width": "25%",
-        "transition": "500ms width linear 1s"
-    });
-    /* second column */
-    setTimeout(function () {
-        $("#partOneColumn2").css({
-            "width": "25%",
-            "transition": "500ms width linear .5s",
-            "visibility": "visible",
-        });
-    }, 2500);
-    /* third column */
-    setTimeout(function () {
-        $("#partOneColumn3").css({
-            "width": "25%",
-            "transition": "500ms width ease-in 1s",
-            "visibility": "visible",
-        });
-    }, 5000);
-    /* fourth column */
-    setTimeout(function () {
-        $("#partOneColumn4").css({
-            "width": "25%",
-            "transition": "500ms width linear .5s",
-            "visibility": "visible",
-        });
-    }, 7000);
-
+    $(".introductionImageChap1").fadeIn(8000);
+    $(".dialogueBoxStripe").fadeIn(5000);
+    $(".dialogueBoxStripe").fadeIn(5000);
 })
 
