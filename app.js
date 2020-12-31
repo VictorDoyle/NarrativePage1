c8c61f55600224eb24e844a32504c0570e0d4387
victordoyle <victor-doyle@outlook.com>
2020-12-31 15:38:29 -0500

    chapter 1 intro done

--- app.js
+++ app.js
@@ -3,8 +3,11 @@
 
 /* NOTE: Hidden Elements pre-loaded */
 $(".chapter1PartOne").hide();
-$(".dialogueBoxStripe").hide()
-$(".introductionImageChap1").hide()
+$(".chapter1PartTwo").hide();
+$("#purpleQuestionOne").hide();
+$("#purpleAnswerOne").hide();
+$("#purpleQuestionTwo").hide();
+$("#purpleAnswerTwo").hide();
 
 /* added game soundtrack to work on click */
 let chapter1Soundtrack = document.getElementById("purpleSong");
@@ -14,12 +17,21 @@ function playAudio() {
     chapter1Soundtrack.play();
 }
 
+/* First question */
 $(".beginChapter1").on("click", function fadeInChapter1(){
     playAudio();
     $(".chapter1Intro").fadeOut(1000);
-    $(".chapter1PartOne").fadeIn(1000);
-    $(".introductionImageChap1").fadeIn(8000);
-    $(".dialogueBoxStripe").fadeIn(5000);
-    $(".dialogueBoxStripe").fadeIn(5000);
+    $(".chapter1PartOne").fadeIn(3000);
+    $("#purpleQuestionOne").delay(1000).fadeIn(7000);
+    $("#purpleAnswerOne").delay(7000).fadeIn(1000);
+})
+
+/* Second Question */
+$("#purpleAnswerOne").on("click", function fadeInChapter2(){
+    $(".chapter1PartOne").fadeOut(3000);
+    $("#purpleIntro").fadeOut(3000);
+    $(".chapter1PartTwo").fadeIn(3000);
+    $("#purpleQuestionTwo").delay(1000).fadeIn(7000);
+    $("#purpleAnswerTwo").delay(7000).fadeIn(1000);
 })
 
